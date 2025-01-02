90153de02c294e31437b660e94db2f551a58c038
J Armando C <armjorge@gmail.com>
2025-01-02 00:13:10 -0600

    Set up virtual environment and add Hello World script

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,9 +1,13 @@
+import pandas as pd
 
-def helloFunction():
-    print("Hello World")
-    
-def main(): 
-    
-    helloFunction()
-    
-    
+def printdataframe(message, rows):
+    df = pd.DataFrame()  # Create an empty DataFrame
+    df['Column One'] = [message] * rows  # Repeat the message
+    return df
+
+def main():
+    outputdf = printdataframe("Hello", 5)
+    print(outputdf)
+
+if __name__ == "__main__":
+    main()
